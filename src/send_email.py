2b97a54c713f2e80faba04fb16e05dc750ff7c86
LuxJakob <112105354+LuxJakob@users.noreply.github.com>
2024-09-12 10:26:32 +0200

    added daily quote API - credit: https://theysaidso.com/api

--- src/send_email.py
+++ src/send_email.py
@@ -6,6 +6,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from typing import List
 
+import requests
+
 
 def send_email(list_of_people: List) -> None:
     port = 465
@@ -20,7 +22,7 @@ def send_email(list_of_people: List) -> None:
     today = datetime.today()
 
     subject = ""
-    message = ""
+    message_body = ""
 
     for entry in list_of_people:
         first_name = entry['First Name']
@@ -30,16 +32,28 @@ def send_email(list_of_people: List) -> None:
         age = today.year - birthday.year
 
         subject += f'{first_name} {last_name} '
-        message += f'{first_name} wird heute {age} Jahre alt! \n\n'
+        message_body += f'{first_name} wird heute {age} Jahre alt! <br><br>'
 
     subject += "hat heute Geburtstag!"
+
+    message_body += 'Da wünsche ich alles Gute zum Geburtstag!<br>'
+    message_body += fetch_quote()
+
+    footer = '''<br><br>
+    <span style="z-index:50;font-size:0.9em; font-weight: bold;">
+        <img src="https://theysaidso.com/branding/theysaidso.png" height="20" width="20" alt="theysaidso.com"/>
+        <a href="https://theysaidso.com" title="Powered by quotes from theysaidso.com" 
+           style="color: #ccc; margin-left: 4px; vertical-align: middle;">They Said So®</a>
+    </span>'''
+
+    message_body += footer
 
     email_message = MIMEMultipart()
     email_message['From'] = username
     email_message['To'] = username
     email_message['Subject'] = subject
 
-    email_message.attach(MIMEText(message, 'plain'))
+    email_message.attach(MIMEText(message_body, 'html'))
 
     try:
         context = ssl.create_default_context()
@@ -51,3 +65,36 @@ def send_email(list_of_people: List) -> None:
         print(f"Authentication error: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def fetch_quote() -> str:
+    api_token_qod = os.environ.get('API_TOKEN_QOD')
+    url = 'https://quotes.rest/qod?category=inspire&language=en'
+
+    headers = {
+        'accept': 'application/json',
+        'Authorization': f'Bearer {api_token_qod}'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    final_quote = '''
+        <p><strong>Bertolt Brecht:</strong></p>
+        <blockquote style="margin-left: 20px;">
+            <p>Und der Haifisch, der hat Zähne<br>
+            Und die trägt er im Gesicht<br>
+            Und Macheath, der hat ein Messer<br>
+            Doch das Messer sieht man nicht.</p>
+        </blockquote>
+    '''
+
+    if response.status_code == 200:
+        data = response.json()
+
+        quote_info = data['contents']['quotes'][0]
+        author = quote_info['author']
+        quote = quote_info['quote']
+
+        final_quote = f'<br><br>{author}:<br>"{quote}<br><br>"'
+
+    return final_quote
